7e26ae83fc9e1c183ba44c394d11a74554abc3c7
JaviPege <ivaj86@gmail.com>
2025-08-18 14:34:30 +0200

    Fix curl download - add -f flag

--- public/install.sh
+++ public/install.sh
@@ -122,7 +122,7 @@ download_and_execute() {
     # Download private installer
     local download_url="https://raw.githubusercontent.com/${PRIVATE_REPO}/main/${PRIVATE_INSTALLER_PATH}"
     
-    if ! curl -H "Authorization: token ${token}" -o "$installer_path" "$download_url" 2>/dev/null; then
+    if ! curl -f -H "Authorization: token ${token}" -o "$installer_path" "$download_url"; then
         log "ERROR" "Error al descargar el instalador"
         rm -rf "$temp_dir"
         return 1
